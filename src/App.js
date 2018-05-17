04d4cb7624326189c84a52fea50f166d20e15ac4
Graham Colgate <graham@gramtech.co.uk>
2018-05-17 20:38:57 +0100

    added AppD dependenies and require

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+require("appdynamics").profile({
+  controllerHostName: 'amelia2018022813480025.saas.appdynamics.com',
+  controllerPort: 443, 
+  controllerSslEnabled: true,  // Set to true if controllerPort is SSL
+  accountName: 'amelia2018022813480025',
+  accountAccessKey: 'z6cr4suam9b1',
+  applicationName: 'GRAMTECH',
+  tierName: 'simple-nodejs-appd', 
+  nodeName: 'simple-nodejs-appd:0' 
+ });
+ 
 class App extends Component {
   render() {
     return (
